bf70db401365b5940b80d45a9942d34f11bda33c
tiar-afk <72110495+tiar-afk@users.noreply.github.com>
2020-11-18 19:15:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://arunika-music.glitch.me"]
+const urls = ["web"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
